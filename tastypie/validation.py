c826672bf8903b647e06d731856349bf0c97d548
Claudio Baccigalupo <claudio.baccigalupo@ff0000.com>
2011-09-26 15:46:07 -0700

    Fixes https://github.com/toastdriven/django-tastypie/issues/152

--- tastypie/validation.py
+++ tastypie/validation.py
@@ -98,3 +98,72 @@ class CleanedDataFormValidation(FormValidation):
         # The data is invalid. Let's collect all the error messages & return
         # them.
         return form.errors
+
+
+###############################################################################
+
+# The following is to fix a bug in tastypie
+# see https://github.com/toastdriven/django-tastypie/issues/152
+
+from django.forms.models import ModelChoiceField
+
+class ModelFormValidation(FormValidation):
+    """
+    Override tastypie's standard ``FormValidation`` since this does not care
+    about URI to PK conversion for ``ToOneField`` or ``ToManyField``.
+    """
+
+    def uri_to_pk(self, uri):
+        """
+        Returns the integer PK part of a URI.
+
+        Assumes ``/api/v1/resource/123/`` format. If conversion fails, this just
+        returns the URI unmodified.
+
+        Also handles lists of URIs
+        """
+
+        if uri is None:
+            return None
+
+        # convert everything to lists
+        multiple = not isinstance(uri, basestring)
+        uris = uri if multiple else [uri]
+
+        # handle all passed URIs
+        converted = []
+        for one_uri in uris:
+            try:
+                if one_uri[-1] == '/': # /api/v1/<resource_name>/<pk>/
+                    converted.append(int(one_uri.split('/')[-2]))
+                else: # /api/v1/<resource_name>/<pk>
+                    converted.append(int(one_uri.split('/')[-1]))
+            except (IndexError, ValueError):
+                raise ValueError(
+                    "URI %s could not be converted to PK integer." % one_uri)
+
+        # convert back to original format
+        return converted if multiple else converted[0]
+
+    def is_valid(self, bundle, request=None):
+        data = bundle.data
+        # Ensure we get a bound Form, regardless of the state of the bundle.
+        if data is None:
+            data = {}
+        # copy data, so we don't modify the bundle
+        data = data.copy()
+
+        # convert URIs to PK integers for all relation fields
+        relation_fields = [name for name, field in
+                           self.form_class.base_fields.items()
+                           if issubclass(field.__class__, ModelChoiceField)]
+
+        for field in relation_fields:
+            if field in data:
+                data[field] = self.uri_to_pk(data[field])
+
+        # validate and return messages on error
+        form = self.form_class(data)
+        if form.is_valid():
+            return {}
+        return form.errors
